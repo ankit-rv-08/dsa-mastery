6c9d354566645471a5e96f87eaab3073e85aadf0
ankit-rv-08 <ankith8804@gmail.com>
2025-06-03 00:47:59 +0530

    Create hour6_linear_search.py

---
+++ arrays/hour6_linear_search.py
@@ -0,0 +1,20 @@
+"""
+Hour 6: Linear Search (Python + DSA)
+Author: Ankith
+Description: Implements linear search to find a target in a list.
+"""
+
+numbers = input("Enter numbers separated by spaces: ").split()
+numbers = [int(num) for num in numbers]
+
+target = int(input("Enter the number to search for: "))
+
+found = False
+for i in range(len(numbers)):
+    if numbers[i] == target:
+        print(f"Number is found at index {i}")
+        found = True
+        break
+
+if not found:
+    print("Number not found in the list.")
