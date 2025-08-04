7f55e31b4b3da41877fea42d5ee825868b9eec8f
ankit-rv-08 <ankith8804@gmail.com>
2025-08-04 22:46:45 +0530

    Create hour3_merge_sort.py

---
+++ arrays/hour3_merge_sort.py
@@ -0,0 +1,40 @@
+"""
+Hour 3 - Merge Sort
+
+Efficient O(n log n) divide and conquer sorting.
+"""
+
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = j = k = 0
+
+        while i < len(left) and j < len(right):
+            if left[i] < right[j]:
+                arr[k] = left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+            k += 1
+
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+if __name__ == "__main__":
+    arr = [12, 11, 13, 5, 6, 7]
+    merge_sort(arr)
+    print("Sorted array is:", arr)
