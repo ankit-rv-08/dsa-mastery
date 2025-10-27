48d8524dd261ea9c7cb286edfb76037defbec999
ankit-rv-08 <ankith8804@gmail.com>
2025-10-27 20:36:58 +0530

    Create valid_palindrome.py

---
+++ Arrays/valid_palindrome.py
@@ -0,0 +1,22 @@
+"""
+LeetCode #125: Valid Palindrome
+Pattern: Two Pointers
+Date: Oct 26, 2025
+"""
+
+def isPalindrome(s):
+    left, right = 0, len(s) - 1
+    
+    while left < right:
+        while left < right and not s[left].isalnum():
+            left += 1
+        while left < right and not s[right].isalnum():
+            right -= 1
+        
+        if s[left].lower() != s[right].lower():
+            return False
+        
+        left += 1
+        right -= 1
+    
+    return True
